aa5491d50ca2be23be3edbcc106c83b5552819da
x0tic0 <87556649+x0tic0@users.noreply.github.com>
2022-02-07 09:36:21 -0500

    Update Skrillec_NET.cs

--- Skrillec Panel/Skrillec_NET.cs
+++ Skrillec Panel/Skrillec_NET.cs
@@ -3,12 +3,40 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Windows.Forms;
+using Skrillec_Panel;
 
+
+class Buffer
+{
+    public static string data;
+    public static string cmd;
+    public static string[] cmd_args;
+
+    public static void cmd_parser(string cmd)
+    {
+        if(cmd.Contains(" "))
+        {
+            Buffer.data = cmd;
+            Buffer.cmd_args = cmd.Split(' ');
+            Buffer.cmd = Buffer.cmd_args[0];
+        } else
+        {
+            Buffer.data = cmd;
+            Buffer.cmd = cmd;
+            Array.Resize(ref Buffer.cmd_args, (Buffer.cmd_args.Length+1));
+            Buffer.cmd_args[Buffer.cmd_args.Length] = (cmd);
+        }
+    }
+}
 class Skrillec_NET
 {
     public static TcpListener server;
     public static IPAddress server_ip = IPAddress.Parse("127.0.0.1");
     public static Int32 port;
+    public static bool server_on = false;
+    public static int tid = 0;
+    public static string server_logs = "";
 
     public static void start_skrillec()
     {
@@ -18,71 +46,62 @@ class Skrillec_NET
             Skrillec_NET.server = new TcpListener(Skrillec_NET.server_ip, Skrillec_NET.port);
 
             // Start listening for client requests.
+            Skrillec_NET.server_logs += "Server started\n";
             Skrillec_NET.server.Start();
+            Skrillec_NET.server_on = true;
+            Skrillec_NET.connection_handler();
 
         }
         catch (SocketException e)
         {
-            Console.WriteLine("SocketException: {0}", e);
+            Skrillec_NET.server_logs += "SocketException: " + e.ToString() + "\n";
         }
         finally
         {
             // Stop listening for new clients.
             Skrillec_NET.server.Stop();
         }
-
-        Console.WriteLine("\nHit enter to continue...");
-        Console.Read();
     }
 
-    public static void connection_handler(TcpClient socketn)
+    public static void connection_handler()
     {
-        // Buffer for reading data
-        Byte[] bytes = new Byte[256];
-        String data = null;
 
         // Enter the listening loop.
         while (true)
         {
-            Console.Write("Waiting for a connection... ");
+            Skrillec_NET.server_logs += "Waiting for a connection... \n";
 
             // Perform a blocking call to accept requests.
             // You could also use server.AcceptSocket() here.
             TcpClient client = Skrillec_NET.server.AcceptTcpClient();
-            Console.WriteLine("Connected!");
+            Skrillec_NET.server_logs += "Connected!\n";
 
-            data = null;
-
-            // Get a stream object for reading and writing
-            NetworkStream stream = client.GetStream();
-
-            int i;
-
-            stream.Write(System.Text.Encoding.ASCII.GetBytes(">>> "), 0, ">>> ".Length);
-            // Loop to receive all the data sent by the client.
-            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-            {
-                // Translate data bytes to a ASCII string.
-                data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                Console.WriteLine("Received: {0}", data);
-
-                // Process the data sent by the client.
-                data = data.ToUpper();
-
-                byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-
-                // Send back a response.
-                stream.Write(msg, 0, msg.Length);
-                Console.WriteLine("Sent: {0}", data);
-            }
-
-            // Shutdown and end connection
-            client.Close();
+            Skrillec_NET.cmd_handler(client);
         }
     }
 
     public static void cmd_handler(TcpClient socket)
     {
+        Byte[] bytes = new Byte[256];
+        String data = null;
+        data = null;
 
+        NetworkStream stream = socket.GetStream();
+        int i;
+
+        stream.Write(System.Text.Encoding.ASCII.GetBytes(">>> "), 0, ">>> ".Length);
+        while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+        {
+            data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+            Skrillec_NET.server_logs += "Received: " + data + "\n";
+
+            if (data == "help" || data == "?")
+            {
+
+            }
+            // Convert Message to byte then send
+            //byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+            //stream.Write(msg, 0, msg.Length);
+        }
     }
 }
